aa7657c52e4cf257988f4fae735a76064a5711f9
T-107 <T-107@PC268>
2015-08-13 19:32:51 -0500

    Alfredo Serrano Examen_Final

---
+++ ExamenFinalAlfredoSerrano/src/examenfinalalfredoserrano/InicializarThread.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package examenfinalalfredoserrano;
+
+/**
+ *
+ * @author T-107
+ */
+public class InicializarThread {
+    public static void main (String [] args) {
+        ThreadInterno t1=new ThreadInterno();
+        t1.start();
+    }
+    
+}
